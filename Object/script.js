85786784194c776b046cd704d7ad500fe647f25f
Aleena Irfan <aleenaazqa8@gmail.com>
2022-01-26 18:45:53 +0500

    PIAIC-Advance-JS-Assignments

--- Object/script.js
+++ Object/script.js
@@ -1,1 +1,9 @@
-
+function submit(){
+    let firstnameInput= document.querySelector("#Firstname");
+    console.log(firstnameInput);
+    console.log(firstnameInput.value);
+    let userDetails={
+        firstName: firstnameInput.value
+    }
+    console.log(userDetails)
+}
